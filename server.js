748ad2bf63255390544a982562d07603c3dd4b2e
sow4063 <sow4063@gmail.com>
2017-02-24 17:44:34 +0900

    modify server.js

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require('express');
-//const router = require('./server/routes/router.js')
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
 
-const app = express();
-const port = 8080;
+// configuration ===========================================
+var port = process.env.PORT || 8080; // set our port
 
-app.use('/', express.static(__dirname + '/dist'));
+// get all data/stuff of the body (POST) parameters
+app.use(bodyParser.json()); // parse application/json 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
-//
-const songController = require('./server/db/song/songController.js');
-const bodyParser = require('body-parser');
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(express.static(__dirname + '/dist')); // set the static files location /public/img will be /img for users
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// routes ==================================================
+require('./server/routes')(app); // pass our application into our routes
 
-app.post('/insertsong', songController.insertSong );
-app.post('/removesong', songController.removeSong );
-app.get('/searchsong', songController.searchSong );
-app.get('/downlaodsong', songController.downloadSong );
-//
+// start app ===============================================
+app.listen(port);	
+console.log('Magic happens on port ' + port); 			// shoutout to the user
 
-app.listen(port, () => console.log('Express listening on port', port))
-
-module.exports = app;
+exports = module.exports = app; 						// expose app
---
+++ server/routes.js
@@ -0,0 +1,16 @@
+var songController = require('./db/song/songController.js');
+
+module.exports = function(app) {
+
+	// server routes ===========================================================
+	// handle things like api calls
+	// authentication routes
+	app.get('/searchsong', songController.searchSong );
+	
+	// frontend routes =========================================================
+	// route to handle all angular requests
+	app.get('*', function(req, res) {
+	  res.sendfile('./client/index.html');
+	});
+
+};
